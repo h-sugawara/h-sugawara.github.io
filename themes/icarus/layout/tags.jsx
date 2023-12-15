049d73f22c093e8878a4f255756e4793bf325efc
Hiroki Sugawara <h-sugawara@users.noreply.github.com>
2023-12-15 19:03:48 +0900

    タグページを登録記事数順で並ぶように変更

--- themes/icarus/layout/tags.jsx
+++ themes/icarus/layout/tags.jsx
@@ -3,8 +3,12 @@ const Tags = require('hexo-component-inferno/lib/view/widget/tags');
 
 module.exports = class extends Component {
     render() {
-        const { site, helper } = this.props;
+        const { site, config, helper } = this.props;
 
-        return <Tags.Cacheable site={site} helper={helper} />;
+        const widget = (config.widgets || []).filter(widget => widget.type === 'tags')
+            .map(widget => { return { order_by: widget.order_by, show_count: widget.show_count }; })
+            .shift() || {};
+
+        return <Tags.Cacheable site={site} helper={helper} widget={widget} />;
     }
 };
