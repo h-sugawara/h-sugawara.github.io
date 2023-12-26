da4336bd2546e8ea19db8934f877e8beb77641e9
Hiroki Sugawara <h-sugawara@users.noreply.github.com>
2023-12-26 20:44:46 +0900

    Site updated: 2023-12-26 20:44:44

--- js/main.js
+++ js/main.js
@@ -1,22 +1,5 @@
 /* eslint-disable node/no-unsupported-features/node-builtins */
 (function ($) {
-    $('.og-image img').each(function () {
-        // 記事のリンクプレビュー画像のイメージエレメントに、遅延ロード属性と代替テキスト属性を付与（プラグインアップデート後に削除）
-        $(this).prop('loading', 'lazy');
-        $(this).attr('alt', $('.og-title').text());
-    });
-
-    /*$('.article img:not(".not-gallery-item")').each(function () {
-        // wrap images with link and add caption if possible
-        if ($(this).parent('a').length === 0) {
-            $(this).wrap('<a class="gallery-item" href="' + $(this).attr('src') + '"></a>');
-            if (this.alt) {
-                $(this).after('<p class="has-text-centered is-size-6 caption">' + this.alt + '</p>');
-            }
-            $(this).prop('loading', 'lazy');
-        }
-    });*/
-
     $('.article > .content > table').each(function () {
         if ($(this).width() > $(this).parent().width()) {
             $(this).wrap('<div class="table-overflow"></div>');
