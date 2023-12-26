ce378ac64b4dafc33c97fc523c1894a50268fc6b
Hiroki Sugawara <h-sugawara@users.noreply.github.com>
2023-12-26 20:40:02 +0900

    記事メディアコンポーネントのリファクタ

--- themes/icarus/layout/common/article_media.jsx
+++ themes/icarus/layout/common/article_media.jsx
@@ -3,14 +3,15 @@ const { Component } = require('inferno');
 module.exports = class extends Component {
     render() {
         const { url, title, date, dateXml, categories, thumbnail } = this.props;
+
         return <article className="media">
             {thumbnail ? <figure className="media-left">
                 <a className="image" href={url}><img src={thumbnail} alt={title} loading="lazy" /></a>
             </figure> : null}
             <div className="media-content">
-                <p className="date"><time dateTime={dateXml}>{date}</time></p>
-                <p className="title"><a href={url}>{title}</a></p>
-                {categories.length ? <p className="categories">{categories.join(' / ')}</p> : null}
+                <time className="date" dateTime={dateXml}>{date}</time>
+                <a className="title" href={url}>{title}</a>
+                {categories.length > 0 && <p className="categories">{categories.join(' / ')}</p>}
             </div>
         </article>;
     }
