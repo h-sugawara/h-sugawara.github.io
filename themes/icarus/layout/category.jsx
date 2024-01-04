2098cf5657d2c7b769cc028b7bf76bb7118055bc
Hiroki Sugawara <h-sugawara@users.noreply.github.com>
2024-01-04 17:03:48 +0900

    カテゴリーレイアウトコンポーネントをリファクタ

--- themes/icarus/layout/category.jsx
+++ themes/icarus/layout/category.jsx
@@ -5,6 +5,7 @@ module.exports = class extends Component {
     render() {
         const { config, page, helper } = this.props;
         const { url_for, _p } = helper;
+        const { parents, category } = page;
 
         return <Fragment>
             <div className="card">
@@ -12,10 +13,8 @@ module.exports = class extends Component {
                     <nav className="breadcrumb" aria-label="breadcrumbs">
                         <ul>
                             <li><a href={url_for('/categories/')}>{_p('common.category', Infinity)}</a></li>
-                            {page.parents.map(category => {
-                                return <li><a href={url_for(category.path)}>{category.name}</a></li>;
-                            })}
-                            <li className="is-active"><a href="#" aria-current="page">{page.category}</a></li>
+                            {parents.map(({path, name}) => <li><a href={url_for(path)}>{name}</a></li>)}
+                            <li className="is-active"><a href="#" aria-current="page">{category}</a></li>
                         </ul>
                     </nav>
                 </div>
