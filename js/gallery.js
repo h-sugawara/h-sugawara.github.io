1ad14c575ab9c9163a4a39a91faee46a89be2722
Hiroki Sugawara <h-sugawara@users.noreply.github.com>
2024-01-12 13:18:30 +0900

    Site updated: 2024-01-12 13:18:27

--- js/gallery.js
+++ js/gallery.js
@@ -1,12 +1,1 @@
-/* eslint-disable node/no-unsupported-features/node-builtins */
-(function ($) {
-    if (typeof $.fn.lightGallery === 'function') {
-        $('.article').lightGallery({selector: '.gallery-item'});
-    }
-    if (typeof $.fn.justifiedGallery === 'function') {
-        if ($('.justified-gallery > p > .gallery-item').length) {
-            $('.justified-gallery > p > .gallery-item').unwrap();
-        }
-        $('.justified-gallery').justifiedGallery();
-    }
-}(jQuery));
+!function(e){"function"==typeof e.fn.lightGallery&&e(".article").lightGallery({selector:".gallery-item"}),"function"==typeof e.fn.justifiedGallery&&(e(".justified-gallery > p > .gallery-item").length&&e(".justified-gallery > p > .gallery-item").unwrap(),e(".justified-gallery").justifiedGallery())}(jQuery);
