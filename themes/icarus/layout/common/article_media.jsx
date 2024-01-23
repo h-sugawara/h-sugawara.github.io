e909a1139b230f27a8df860d909b88f6f26a0047
Hiroki Sugawara <h-sugawara@users.noreply.github.com>
2024-01-23 20:42:02 +0900

    記事メディアコンポーネント及びスタイルのリファクタ

--- themes/icarus/layout/common/article_media.jsx
+++ themes/icarus/layout/common/article_media.jsx
@@ -7,9 +7,9 @@ module.exports = class extends Component {
         return <a className="media link-muted" href={url}>
             {thumbnail ? <figure className="media-left image"><img src={thumbnail} alt={title} loading="lazy" /></figure> : null}
             <div className="media-content">
-                <time className="date" dateTime={dateXml}>{date}</time>
+                <time dateTime={dateXml}>{date}</time>
                 <h4 className="title">{title}</h4>
-                {categories.length > 0 && <p className="categories">{categories.join(' / ')}</p>}
+                {categories.length > 0 && categories.join(' / ')}
             </div>
         </a>;
     }
