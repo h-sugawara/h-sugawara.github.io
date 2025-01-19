715f7b1964c079eade180bf4803b604f686506f3
Hiroki Sugawara <h-sugawara@users.noreply.github.com>
2025-01-19 15:12:37 +0900

    記事アイコンやリンクプレビューの画像がレイアウトシフトしないように修正

--- themes/icarus/layout/common/article_media.jsx
+++ themes/icarus/layout/common/article_media.jsx
@@ -5,7 +5,7 @@ module.exports = class extends Component {
         const { url, title, date, dateXml, categories, thumbnail } = this.props;
 
         return <a className="media" href={url}>
-            {thumbnail ? <figure className="media-left image"><img src={thumbnail} alt="" loading="lazy" /></figure> : null}
+            {thumbnail ? <figure className="media-left image"><img src={thumbnail} alt="" loading="lazy" width="64" height="64" /></figure> : null}
             <div className="media-content">
                 <time dateTime={dateXml}>{date}</time>
                 <h4 className="title">{title}</h4>
